843c44281833a632ae9e0440064d6968e0ee3caf
rohinp <rohin.patel@outlook.com>
2016-03-02 12:52:02 +0530

    updated jenkins file to show reports before staging stage

---
+++ src/test/java/org/jenkinsci/plugins/workflow/demo/AppTest.java
@@ -0,0 +1,25 @@
+package org.jenkinsci.plugins.workflow.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest{
+
+    @Test
+    public void itShouldTestApp1() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void itShouldTestApp2() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void itShouldTestApp3() {
+        assertTrue(true);
+    }
+
+
+}
